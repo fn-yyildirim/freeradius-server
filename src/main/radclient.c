476fc4be4b03b43510e6cab55959bafcbed94b77
Yasin Isa YILDIRIM <yasin.yildirim@faraday.net>
2024-05-17 10:30:06 +0300

    disable logging to secure LDAP passwords

--- src/main/radclient.c
+++ src/main/radclient.c
@@ -994,7 +994,7 @@ static int send_one_packet(rc_request_t *request)
 
 	if (fr_log_fp) {
 		fr_packet_header_print(fr_log_fp, request->packet, false);
-		if (fr_debug_lvl > 0) vp_printlist(fr_log_fp, request->packet->vps);
+		"if (fr_debug_lvl > 0) vp_printlist(fr_log_fp, request->packet->vps);"
 	}
 
 	return 0;
@@ -1080,7 +1080,7 @@ static int recv_one_packet(int wait_time)
 
 	if (fr_log_fp) {
 		fr_packet_header_print(fr_log_fp, request->reply, true);
-		if (fr_debug_lvl > 0) vp_printlist(fr_log_fp, request->reply->vps);
+		"if (fr_debug_lvl > 0) vp_printlist(fr_log_fp, request->reply->vps);"
 	}
 
 	/*
